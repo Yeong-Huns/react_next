6b2d10d1e537a0a06e23754be26208683b4a85ea
Yeong-huns <vosxja1@gmail.com>
2025-01-28 04:04:12 +0900

    feat/supabaseInsert&Select

--- components/common/navigation/SideNavigation.tsx
+++ components/common/navigation/SideNavigation.tsx
@@ -1,8 +1,60 @@
+'use client'
+
 import {Button} from "@/components/ui/button";
-import {Search} from "lucide-react";
+import {Dot, Search} from "lucide-react";
 import {Input} from "@/components/ui/input";
+import {useRouter} from "next/navigation";
+import {supabase} from "@/utils/supabase";
+import {toast} from "@/hooks/use-toast";
+import {useEffect, useState} from "react";
+
+type Todo = {
+    title: string;
+    start_date: Date;
+    end_date: Date;
+    content: string;
+}
 
 export const SideNavigation = () => {
+    const router = useRouter();
+    const [todos, setTodos] = useState<any>([]);
+
+    const onCreate = async () => {
+        console.log('onCreate');
+
+        const {error, status} = await supabase.from('todos').insert([
+            {
+                title: "",
+                start_date: "",
+                end_date: "",
+                content: ""
+            },
+        ])
+
+        if (error) {
+            console.log('error', error);
+        }
+        if (status === 201) {
+            toast({
+                title: "생성 완료",
+                description: "새로운 TodoList 생성에 성공하였습니다."
+            })
+            router.push('/create')
+        }
+    }
+
+    const getTodos = async () => {
+        const {data, error, status} = await supabase.from('todos').select('*');
+
+        if (status === 200) {
+            setTodos(data);
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, [])
+
     return <div className={'flex flex-col w-[280px] h-screen py-5 px-6 border-x border-gray-200 gap-1'}>
         <div className={'flex items-center justify-start gap-1'}>
             <Input type={'text'} placeholder={'검색어를 입력해주세요'} className={'focus-visible:ring-0'}/>
@@ -11,10 +63,24 @@ export const SideNavigation = () => {
             </Button>
         </div>
         <div className={'flex items-center justify-center w-full my-3'}>
-            <Button variant={"outline"} className={'w-full text-orange-500 border-orange-400 hover:bg-orange-50 hover:text-orange-500'}>Add New Page</Button>
+            <Button variant={"outline"}
+                    onClick={onCreate}
+                    className={'w-full text-orange-500 border-orange-400 hover:bg-orange-50 hover:text-orange-500'}>Add New Page</Button>
         </div>
         <div className={'flex flex-col w-full'}>
             <span className={'my-2 font-medium text-sm text-gray-400'}>나의 할 일</span>
+            {/*TodoList*/}
+            <div className={'flex flex-col gap-2'}>
+            {todos &&
+                todos.map((item: any) => {
+                    return (
+                        <div className={'flex items-center py-2 bg-gray-100 rounded-sm cursor-pointer'} key={item.id}>
+                            <Dot className={'mr-1 text-green-500'}></Dot>
+                            <span className={'text-sm'}>{item.title === "" ? "제목 없음" : item.title}</span>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     </div>
 }
--- components/dialog/MarkdownDialog.tsx
+++ components/dialog/MarkdownDialog.tsx
@@ -6,12 +6,55 @@ import {Checkbox} from "@/components/ui/checkbox";
 import {LabelCalendar} from "@/components/calendar/LabelCalendar";
 import {Separator} from "@/components/ui/separator";
 import MDEditor from "@uiw/react-md-editor";
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
+import {useToast} from "@/hooks/use-toast";
+import {supabase} from "@/utils/supabase";
 
 export const MarkdownDialog = () => {
+    const [open, setOpen] = useState<boolean>(false);
     const [contents, setContents] = useState<string | undefined>("**마크다운 문법을 지원합니다!**");
+    const { toast } = useToast()
+    const [title, setTitle] = useState<string>("")
 
-    return <Dialog>
+    /*
+    *
+    * */
+    const onSubmit = async () => {
+        console.log("onSubmit")
+        if(!title || !contents) {
+            toast({
+                title: "입력되지 않은 필수 요소가 존재합니다.",
+                description: "제목, 날짜, 콘텐츠 내용을 모두 입력해주세요.",
+            });
+            return;
+        } else {
+            // SUPABASE 연동
+            const { data, error, status } = await supabase
+                .from('todos')
+                .insert([
+                    { title: title, content: contents },
+                ])
+                .select()
+            if(error){
+                console.log(error)
+                toast({
+                    title: "저장에 실패하였습니다.",
+                    description: "데이터 저장중 서버 오류가 발생했습니다."
+                })
+            }
+            if(status === 201){
+                toast({
+                    title: "저장 완료",
+                    description: "성공적으로 저장되었습니다!"
+                })
+
+                // 추가 초기화 로직
+                setOpen(false);
+            }
+        }
+    }
+
+    return <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
             <Button variant={'ghost'} className={'font-normal text-gray-400 hover:text-gray-500 cursor-pointer'}>추가</Button>
         </DialogTrigger>
@@ -21,6 +64,7 @@ export const MarkdownDialog = () => {
                     <div className={'flex items-center justify-start w-[45vw] gap-2'}>{/*타이틀박스*/}
                         <Checkbox className={'w-5 h-5'}/>
                         <input type={'text'} placeholder={'내용을 입력해주세요.'}
+                               onChange={(event:ChangeEvent<HTMLInputElement>)=> setTitle(event.target.value)}
                                className={'w-full outline-none border-none font-medium text-2xl text-gray-600 placeholder:font-medium placeholder-gray-400'}/>
                     </div>
                 </DialogTitle>
@@ -41,6 +85,7 @@ export const MarkdownDialog = () => {
                                 className={'font-normal text-gray-400 hover:bg-gray-50 hover:text-gray-500'}>취소</Button>
                     </DialogClose>
                     <Button type={'submit'}
+                            onClick={onSubmit}
                             className={'font-normal border-orange-500 bg-orange-400 text-white hover:bg-orange-400 hover:text-white'}>
                         작성
                     </Button>
